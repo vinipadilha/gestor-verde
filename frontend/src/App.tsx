ea622be71e37df4bc5e94568d959411f3b9c8296
Vinicius <viniciusdarosapadilha@gmail.com>
2025-08-19 18:59:55 -0300

    Altera nomenclaturas para "Gestão+"

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import PDV from "./pages/PDV";
+import Caixa from "./pages/Caixa";
 import Estoque from "./pages/Estoque";
 import Vendas from "./pages/Vendas";
 import Clientes from "./pages/Clientes";
@@ -20,7 +20,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/pdv" element={<PDV />} />
+          <Route path="/caixa" element={<Caixa />} />
           <Route path="/estoque" element={<Estoque />} />
           <Route path="/vendas" element={<Vendas />} />
           <Route path="/clientes" element={<Clientes />} />
