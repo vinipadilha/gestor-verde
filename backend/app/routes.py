d39685f4bf82a02830f75039430d49157d27c850
Vinicius <viniciusdarosapadilha@gmail.com>
2025-08-20 16:00:58 -0300

    crud exemplo

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,13 +1,57 @@
 # app/routes.py
 
 from app import app
-from flask import jsonify
+from app.models import get_db_connection
+from flask import request, jsonify
 
-@app.route('/')
-def index():
-    return "Bem-vindo ao Backend do Gestor Verde!"
+# Rota para CRIAR (CREATE) um novo produto
+@app.route('/api/produto', methods=['POST'])
+def create_produto():
+    data = request.get_json()
+    nome = data['proNome']
+    quantidade = data['proQtd']
 
-@app.route('/api/score', methods=['GET'])
-def get_score():
-    # Esta rota pode retornar a pontuação do jogo, por exemplo
-    return jsonify({"score": 10})
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO produto (proNome, proQtd) VALUES (%s, %s)", (nome, quantidade))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return jsonify({"message": "Produto criado com sucesso!"}), 201
+
+# Rota para LER (READ) todos os produtos
+@app.route('/api/produto', methods=['GET'])
+def get_produtos():
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM produto")
+    produtos = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return jsonify(produtos)
+
+# Rota para ATUALIZAR (UPDATE) um produto
+@app.route('/api/produto/<int:proId>', methods=['PUT'])
+def update_produto(proId):
+    data = request.get_json()
+    nome = data['proNome']
+    quantidade = data['proQtd']
+
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("UPDATE produto SET proNome = %s, proQtd = %s WHERE proId = %s", (nome, quantidade, proId))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return jsonify({"message": "Produto atualizado com sucesso!"})
+
+# Rota para DELETAR (DELETE) um produto
+@app.route('/api/produto/<int:proId>', methods=['DELETE'])
+def delete_produto(proId):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM produto WHERE proId = %s", (proId,))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return jsonify({"message": "Produto deletado com sucesso!"})
